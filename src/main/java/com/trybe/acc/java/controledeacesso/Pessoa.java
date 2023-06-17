fc31620c4dc230f111ba77e8f0b21e4559784913
Anderson Pereira da Silva <andersonp.silva25@gmail.com>
2023-06-17 13:35:04 -0300

    :sparkles: Feat: Implementing report

--- src/main/java/com/trybe/acc/java/controledeacesso/Pessoa.java
+++ src/main/java/com/trybe/acc/java/controledeacesso/Pessoa.java
@@ -17,4 +17,26 @@ public class Pessoa {
       System.out.println("Pessoa adulta a partir de 50, catraca liberada!");
     }
   }
+
+  /**
+   * Método para gerar o relatorio com a quantidade de pessoas e as porcentagens.
+   */
+  public static void report(int[] count) {
+    double totalPeople = count[0] + count[1] + count[2];
+    double percentageMenores = (count[0] / totalPeople) * 100;
+    double percentageAdultas = (count[1] / totalPeople) * 100;
+    double percentageMaiores = (count[2] / totalPeople) * 100;
+
+    System.out.println("----- Quantidade -----");
+    System.out.println("menores: " + count[0]);
+    System.out.println("adultas: " + count[1]);
+    System.out.println("a partir de 50: " + count[2]);
+
+    System.out.println("----- Percentual -----");
+    System.out.println("menores: " + percentageMenores + "%");
+    System.out.println("adultas: " + percentageAdultas + "%");
+    System.out.println("a partir de 50: " + percentageMaiores + "%");
+
+    System.out.println("Total: " + totalPeople);
+  }
 }
--- src/main/java/com/trybe/acc/java/controledeacesso/Principal.java
+++ src/main/java/com/trybe/acc/java/controledeacesso/Principal.java
@@ -19,11 +19,20 @@ public class Principal {
     System.out.println("2 - Finalizar sistema e mostar relatório");
 
     int option = scanner.nextInt();
+    int[] count = new int[3];
 
     while (option != 2) {
       System.out.println("Entre com a sua idade:");
       short age = scanner.nextShort();
       Pessoa.checkAge(age);
+
+      if (age < 18) {
+        count[0] += 1;
+      } else if (age < 50) {
+        count[1] += 1;
+      } else {
+        count[2] += 1;
+      }
 
       System.out.println("Entre com o número correspondente à opção desejada:");
       System.out.println("1 - Acessar o estabelecimento");
@@ -32,6 +41,7 @@ public class Principal {
       option = scanner.nextInt();
     }
 
+    Pessoa.report(count);
     scanner.close();
   }
 }
